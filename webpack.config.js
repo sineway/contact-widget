0342d753bcba7c3f2ac38bf1c1c00d79a8d6a4a9
Sineway <sineway@yandex.com>
2023-04-04 11:34:17 +0300

    build: rewrite webpack.config.js as class

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,73 @@
-import path from 'node:path';
-import url from 'node:url';
+import {URL} from 'node:url';
 import CopyPlugin from 'copy-webpack-plugin';
 
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+/**
+ * @typedef {import('webpack').Configuration} WebpackConfigInterface
+ * @implements {WebpackConfigInterface}
+ */
+class WebpackConfig {
+  constructor() {
+    this.entry = this.getEntry();
+    this.output = this.getOutput();
+    this.devtool = this.getDevtool();
+    this.plugins = this.getPlugins();
+    this.module = this.getModule();
+  }
 
-export default {
-  entry: './source/main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true,
-  },
-  devtool: 'source-map',
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        {
-          from: 'public',
-        },
-      ],
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-};
+  getEntry() {
+    return './source/main.js';
+  }
+
+  getOutput() {
+    const filename = 'bundle.js';
+    const path = new URL('build', import.meta.url).pathname;
+    const clean = true;
+
+    return {filename, path, clean};
+  }
+
+  getDevtool() {
+    return 'source-map';
+  }
+
+  getPlugins() {
+    const copyPlugin = this.getCopyPlugin();
+
+    return [copyPlugin];
+  }
+
+  getCopyPlugin() {
+    const patterns = [{from: 'public'}];
+
+    return new CopyPlugin({patterns});
+  }
+
+  getModule() {
+    const rules = this.getModuleRules();
+
+    return {rules};
+  }
+
+  getModuleRules() {
+    const js = this.getJsModuleRule();
+    const css = this.getCssModuleRule();
+
+    return [js, css];
+  }
+
+  getJsModuleRule() {
+    const test = /\.js$/;
+    const use = ['babel-loader'];
+
+    return {test, use};
+  }
+
+  getCssModuleRule() {
+    const test = /\.css$/;
+    const use = ['style-loader', 'css-loader'];
+
+    return {test, use};
+  }
+}
+
+export default new WebpackConfig();
